31dcbbd3d686afa5144c387fe1eef21af9bfcd12
Cooley, Rodger <rocooley@utmb.edu>
2018-11-19 16:01:16 -0600

    L63 protect routes with guards, cmd line creating, protect one route

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -16,6 +16,7 @@ import { ListsComponent } from './lists/lists.component';
 import { MessagesComponent } from './messages/messages.component';
 import { RouterModule } from '@angular/router';
 import { appRoutes } from './routes';
+import { AuthGuard } from './_guards/auth.guard';
 
 @NgModule({
 	declarations: [
@@ -34,7 +35,12 @@ import { appRoutes } from './routes';
 		BsDropdownModule.forRoot(),
 		RouterModule.forRoot(appRoutes)
 	],
-	providers: [AuthService, ErrorInterceptorProvider, AlertifyService],
+	providers: [
+		AuthService,
+		ErrorInterceptorProvider,
+		AlertifyService,
+		AuthGuard
+	],
 	bootstrap: [AppComponent]
 })
 export class AppModule {}
