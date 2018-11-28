5cd0531195f907eea34340b6012bf0b3b1de4867
rocooley <rocooley@utmb.edu>
2018-11-28 14:21:45 -0600

    L146-add ordering into SPA, end module 14

--- DatingApp-SPA/src/app/app.module.ts
+++ DatingApp-SPA/src/app/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BsDropdownModule, TabsModule, BsDatepickerModule, PaginationModule } from 'ngx-bootstrap';
+import { BsDropdownModule, TabsModule, BsDatepickerModule, PaginationModule, ButtonsModule } from 'ngx-bootstrap';
 
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
@@ -60,6 +60,7 @@ export function tokenGetter() {
 		BsDatepickerModule.forRoot(),
 		PaginationModule.forRoot(),
 		TabsModule.forRoot(),
+		ButtonsModule.forRoot(),
 		RouterModule.forRoot(appRoutes),
 		NgxGalleryModule,
 		FileUploadModule,
