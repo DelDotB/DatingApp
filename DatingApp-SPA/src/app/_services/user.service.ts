7cd50a7cde2f5a85cdbee057c39d7c0effe93e84
rocooley <rocooley@utmb.edu>
2018-11-29 13:01:03 -0600

    L163-message component SPA, request works

--- DatingApp-SPA/src/app/_services/user.service.ts
+++ DatingApp-SPA/src/app/_services/user.service.ts
@@ -5,6 +5,7 @@ import { Observable } from 'rxjs';
 import { User } from '../_models/user';
 import { PaginatedResult } from '../_models/Pagination';
 import { map } from 'rxjs/operators';
+import { Message } from '../_models/message';
 
 @Injectable({
 	providedIn: 'root'
@@ -14,7 +15,12 @@ export class UserService {
 
 	constructor(private http: HttpClient) {}
 
-	getUsers(page?, itemsPerPage?, userParams?, likesParam?): Observable<PaginatedResult<User[]>> {
+	getUsers(
+		page?,
+		itemsPerPage?,
+		userParams?,
+		likesParam?
+	): Observable<PaginatedResult<User[]>> {
 		const paginatedResult: PaginatedResult<User[]> = new PaginatedResult<
 			User[]
 		>();
@@ -33,11 +39,11 @@ export class UserService {
 			params = params.append('orderBy', userParams.orderBy);
 		}
 
-		if ( likesParam === 'Likers') {
+		if (likesParam === 'Likers') {
 			params = params.append('likers', 'true');
 		}
 
-		if ( likesParam === 'Likees') {
+		if (likesParam === 'Likees') {
 			params = params.append('likees', 'true');
 		}
 
@@ -78,6 +84,42 @@ export class UserService {
 	}
 
 	sendLike(id: number, recipientId: number) {
-		return this.http.post(this.baseUrl + 'users/' + id + '/like/' + recipientId, {});
+		return this.http.post(
+			this.baseUrl + 'users/' + id + '/like/' + recipientId,
+			{}
+		);
+	}
+
+	getMessages(id: number, page?, itemsPerPage?, messageContainer?) {
+		const paginatedResult: PaginatedResult<Message[]> = new PaginatedResult<
+			Message[]
+		>();
+
+		let params = new HttpParams();
+
+		params = params.append('MessageContainer', messageContainer);
+
+		if (page != null && itemsPerPage != null) {
+			params = params.append('pageNumber', page);
+			params = params.append('pageSize', itemsPerPage);
+		}
+
+		return this.http
+			.get<Message[]>(this.baseUrl + 'users/' + id + '/messages', {
+				observe: 'response',
+				params
+			})
+			.pipe(
+				map(response => {
+					paginatedResult.result = response.body;
+					if (response.headers.get('Pagination') !== null) {
+						paginatedResult.pagination = JSON.parse(
+							response.headers.get('Pagination')
+						);
+					}
+
+					return paginatedResult;
+				})
+			);
 	}
 }
